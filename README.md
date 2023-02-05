81f740ec4dfc24dd9469b983f8a3adce57a5657e
AlaaAlhoot <87899347+AlaaAlhoot@users.noreply.github.com>
2023-02-05 17:41:56 +0200

    Final Edit

--- README.md
+++
@@ -1,2 +0,0 @@
-# FlashCard-GSG
-FlashCard project for the advanced Django course with Gaza Sky Geeks
--- cards/urls.py
+++ cards/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
-urlpatterns = [
-    path("", views.CardListView.as_view(), name="card-list"),
-    path("new", views.CardCreateView.as_view(), name="card-create"),
-    path("edit/<int:pk>", views.CardUpdateView.as_view(), name="card-update"),
 
-]
+
+urlpatterns = [
+    path("",views.CardListView.as_view(),name="card-list"),
+    path("new",views.CardCreateView.as_view(),name="card-create"),
+    path("edit/<int:pk>",views.CardUpdateView.as_view(),name="card-update"),
+    path("box/<int:box_num>",views.BoxView.as_view(),name="box"),
+    path("delete/<int:pk>",views.deleteview.as_view(),name="card-delete")
+             ]
